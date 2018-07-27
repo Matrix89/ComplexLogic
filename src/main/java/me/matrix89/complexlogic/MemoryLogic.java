d850d5a909d78053de717199797a8d595528191e
marcin212 <marcin212@protonmail.com>
2018-07-27 19:23:01 +0200

    reimplement Memory logic

--- src/main/java/me/matrix89/complexlogic/MemoryLogic.java
+++ src/main/java/me/matrix89/complexlogic/MemoryLogic.java
@@ -1,17 +1,22 @@
 package me.matrix89.complexlogic;
 
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 import net.minecraft.util.EnumFacing;
 import pl.asie.simplelogic.gates.PartGate;
 
+import java.util.Map;
+
 public class MemoryLogic extends BundledGateLogic {
 
-    private byte[] value = new byte[16];
+    private int address = 0;
+    private Map<Integer, Integer> memory = new Int2IntOpenHashMap();
 
     @Override
     public Connection getType(EnumFacing dir) {
         switch (dir) {
             case NORTH: return Connection.OUTPUT_BUNDLED;
             case EAST: return Connection.INPUT;
+            case WEST: return Connection.INPUT_BUNDLED;
             case SOUTH: return Connection.INPUT_BUNDLED;
             default: return Connection.NONE;
         }
@@ -19,15 +24,16 @@ public class MemoryLogic extends BundledGateLogic {
 
     @Override
     public boolean tick(PartGate parent) {
-        if(getInputValueInside(EnumFacing.WEST) != 0) {
-            value = parent.getBundledInput(EnumFacing.SOUTH);
+        address = bundledRsToDigi(parent.getBundledInput(EnumFacing.WEST));
+        if(getInputValueInside(EnumFacing.EAST) != 0) {
+            memory.put(address, bundledRsToDigi(parent.getBundledInput(EnumFacing.SOUTH)));
         }
         return super.tick(parent);
     }
 
     @Override
     public byte[] calculateBundledOutput(EnumFacing facing) {
-        if(facing == EnumFacing.NORTH) return value;
+        if(facing == EnumFacing.NORTH) return bundledDigiToRs(memory.getOrDefault(address,0));
         return new byte[16];
     }
 
