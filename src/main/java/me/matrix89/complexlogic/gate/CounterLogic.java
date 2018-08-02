6f2ca49952834e53cc3d5cac344aecef132d3344
marcin212 <marcin212@protonmail.com>
2018-08-02 19:13:30 +0200

    add model for counter gate

--- src/main/java/me/matrix89/complexlogic/gate/CounterLogic.java
+++ src/main/java/me/matrix89/complexlogic/gate/CounterLogic.java
@@ -1,11 +1,34 @@
 package me.matrix89.complexlogic.gate;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import pl.asie.simplelogic.gates.PartGate;
 
 public class CounterLogic extends BundledGateLogic {
     private boolean incSignalOn = false;
     private boolean updateSignalOn = false;
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound tag, boolean isClient) {
+        if(!isClient){
+            tag.setBoolean("incSignalOn", incSignalOn);
+            tag.setBoolean("updateSignalOn", updateSignalOn);
+        }
+        return super.writeToNBT(tag, isClient);
+    }
+
+    @Override
+    public boolean readFromNBT(NBTTagCompound compound, boolean isClient) {
+        boolean updateSignalOnOld = updateSignalOn;
+        boolean incSignalOnOld = incSignalOn;
+            if(compound.hasKey("updateSignalOn")){
+                updateSignalOn = compound.getBoolean("updateSignalOn");
+            }
+            if(compound.hasKey("incSignalOn")){
+                incSignalOn = compound.getBoolean("incSignalOn");
+            }
+        return super.readFromNBT(compound, isClient) || updateSignalOnOld!=updateSignalOn || incSignalOnOld!=incSignalOn;
+    }
 
     @Override
     public Connection getType(EnumFacing dir) {
@@ -28,7 +51,9 @@ public class CounterLogic extends BundledGateLogic {
 
     @Override
     public boolean tick(PartGate parent) {
-        parent.updateInputs(this.inputValues);
+        boolean update = parent.updateInputs(this.inputValues);
+        boolean updateSignalOnOld = updateSignalOn;
+        boolean incSignalOnOld = incSignalOn;
         if (getInputValueInside(EnumFacing.WEST) != 0) {
             if(!updateSignalOn){
                 updateSignalOn = true;
@@ -46,12 +71,21 @@ public class CounterLogic extends BundledGateLogic {
             incSignalOn = false;
         }
 
-        return super.tick(parent);
+        return super.tick(parent) || update || updateSignalOnOld!=updateSignalOn || incSignalOnOld!=incSignalOn;
     }
 
     @Override
     public State getLayerState(int i) {
-        return State.ON;
+        switch (i) {
+            case 0: return getInputValueInside(EnumFacing.EAST)!=0? State.ON: State.OFF;
+            case 1: return getInputValueInside(EnumFacing.WEST)!=0? State.ON: State.OFF;
+            default: return State.ON;
+        }
+    }
+
+    @Override
+    public boolean canInvertSide(EnumFacing side) {
+        return side==EnumFacing.EAST;
     }
 
     @Override
