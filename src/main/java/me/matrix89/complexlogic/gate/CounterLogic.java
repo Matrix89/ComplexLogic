e8c777baf720d1920c0f05819a57f091dc08fc70
marcin212 <marcin212@protonmail.com>
2018-08-02 02:25:17 +0200

    fix counter logic

--- src/main/java/me/matrix89/complexlogic/gate/CounterLogic.java
+++ src/main/java/me/matrix89/complexlogic/gate/CounterLogic.java
@@ -5,26 +5,43 @@ import pl.asie.simplelogic.gates.PartGate;
 
 public class CounterLogic extends BundledGateLogic {
     private boolean incSignalOn = false;
+    private boolean updateSignalOn = false;
 
     @Override
     public Connection getType(EnumFacing dir) {
         switch (dir) {
-            case NORTH: return Connection.OUTPUT_BUNDLED;
-            case SOUTH: return Connection.INPUT_BUNDLED;
-            case EAST: return Connection.INPUT;
-            default: return Connection.NONE;
+            case NORTH:
+                return Connection.OUTPUT_BUNDLED;
+            case SOUTH:
+                return Connection.INPUT_BUNDLED;
+            case EAST:
+            case WEST:
+                return Connection.INPUT;
+            default:
+                return Connection.NONE;
         }
+    }
+
+    private int counter(){
+        return isSideInverted(EnumFacing.EAST)? -1: 1;
     }
 
     @Override
     public boolean tick(PartGate parent) {
-/*        if(getInputValueInside(EnumFacing.WEST) != 0) {
-            setBundledValue(EnumFacing.NORTH, getInputValueBundled(EnumFacing.SOUTH));
-        }*/
-        System.out.println(getInputValueInside(EnumFacing.EAST) + " " + incSignalOn);
-        if(getInputValueInside(EnumFacing.EAST) != 0 && !incSignalOn) {
-            incSignalOn = true;
-            setBundledValue(EnumFacing.NORTH, bundledDigiToRs( bundledRsToDigi(getOutputValueBundled(EnumFacing.NORTH)) + 1 ));
+        parent.updateInputs(this.inputValues);
+        if (getInputValueInside(EnumFacing.WEST) != 0) {
+            if(!updateSignalOn){
+                updateSignalOn = true;
+                setBundledValue(EnumFacing.NORTH, getInputValueBundled(EnumFacing.SOUTH));
+            }
+        }else{
+            updateSignalOn = false;
+        }
+        if (getInputValueOutside(EnumFacing.EAST) != 0) {
+            if (!incSignalOn) {
+                incSignalOn = true;
+                setBundledValue(EnumFacing.NORTH, bundledDigiToRs(bundledRsToDigi(getOutputValueBundled(EnumFacing.NORTH)) + counter()));
+            }
         } else {
             incSignalOn = false;
         }
@@ -39,6 +56,10 @@ public class CounterLogic extends BundledGateLogic {
 
     @Override
     public State getTorchState(int i) {
-        return State.ON;
+        switch (i){
+            case 1: return getInputValueInside(EnumFacing.EAST)!=0? State.ON :State.OFF;
+            case 0:
+            default: return State.ON;
+        }
     }
 }
