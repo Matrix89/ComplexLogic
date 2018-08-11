a38ae3379b45de290055fb323e3311352850ed02
marcin212 <marcin212@protonmail.com>
2018-08-11 19:51:43 +0200

    after:charset

--- src/main/java/me/matrix89/complexlogic/ComplexLogic.java
+++ src/main/java/me/matrix89/complexlogic/ComplexLogic.java
@@ -25,7 +25,7 @@ import java.util.Map;
         modid = ComplexLogic.MOD_ID,
         name = ComplexLogic.MOD_NAME,
         version = ComplexLogic.VERSION,
-        dependencies = "before:charset"
+        dependencies = "after:charset"
 )
 public class ComplexLogic {
 
