f8c4156daf3cf6193d3bfde23ed0fd85bf8906af
Matrix89 <matrix8910@gmail.com>
2018-08-14 11:54:57 +0200

    add selection to hex editor

--- src/main/java/me/matrix89/complexlogic/gui/HexEditorGUI.java
+++ src/main/java/me/matrix89/complexlogic/gui/HexEditorGUI.java
@@ -24,6 +24,9 @@ public class HexEditorGUI extends GuiContainerCharset<HexEditorContainer> {
     private int spacing = 2;
     private int charWidth = 3;
 
+    private int selectionStart = 0;
+    private int selectionEnd = 0;
+
     private Random rnd = new Random();
 
     private byte[] data = new byte[32];
@@ -40,10 +43,8 @@ public class HexEditorGUI extends GuiContainerCharset<HexEditorContainer> {
 
     @Override
     protected void keyTyped(char typedChar, int keyCode) throws IOException {
-        System.out.println(keyCode);
         Pattern p = Pattern.compile("[a-fA-F0-9]");
-        System.out.println(cursorNibble);
-        if (p.matcher(String.valueOf(typedChar).toUpperCase()).find()) {
+        if (!isCtrlKeyDown() && p.matcher(String.valueOf(typedChar).toUpperCase()).find()) {
             switch (cursorNibble) {
                 case UPPER:
                     data[cursor] &= (data[cursor] << 8 | 0x0f);
@@ -59,22 +60,71 @@ public class HexEditorGUI extends GuiContainerCharset<HexEditorContainer> {
             }
             return;
         }
+
+        handledMovement(keyCode);
+        super.keyTyped(typedChar, keyCode);
+    }
+
+    private void handledMovement(int keyCode) {
         switch (keyCode) {
             case 205:
+                if (isShiftKeyDown()) {
+                    if (selectionStart == selectionEnd || (cursor < selectionStart || cursor > selectionEnd)) { // create new selection
+                        selectionStart = cursor;
+                        selectionEnd = cursor + 1;
+                    } else if (cursor == selectionStart) {
+                        selectionStart++;
+                    } else if (cursor == selectionEnd) {
+                        selectionEnd++;
+                    }
+                }
                 setCursor(++cursor);
                 break;
             case 203:
+                if (isShiftKeyDown()) {
+                    if (selectionStart == selectionEnd || (cursor < selectionStart || cursor > selectionEnd)) { // create new selection
+                        selectionStart = cursor - 1;
+                        selectionEnd = cursor;
+                    } else if (cursor == selectionEnd) {
+                        selectionEnd--;
+                    } else if (cursor == selectionStart) {
+                        selectionStart--;
+                    }
+                }
                 setCursor(--cursor);
                 break;
             case 200://up
-                setCursor(cursor-(groupSize * groupsPerLine));
+                int startPos = cursor;
+                if (isShiftKeyDown()) {
+                    if (cursor == selectionEnd) {
+                        selectionEnd -= groupSize * groupsPerLine;
+                    } else {
+                        selectionStart -= groupSize * groupsPerLine;
+                    }
+                }
+                setCursor(cursor - (groupSize * groupsPerLine));
                 break;
             case 208://down
-                setCursor(cursor+(groupSize * groupsPerLine));
+                setCursor(cursor + (groupSize * groupsPerLine));
+                if (isShiftKeyDown()) {
+                    if (cursor == selectionStart) {
+                        selectionStart += groupSize * groupsPerLine;
+                    } else {
+                        selectionEnd += groupSize * groupsPerLine;
+                    }
+                }
+                break;
+            case 46: //copy
+                StringBuilder sb = new StringBuilder();
+                for (int i = selectionStart; i <= selectionEnd; i++) {
+                    sb.append(String.format("%02X", data[i]));
+                }
+                setClipboardString(sb.toString());
+                break;
+            case 47: //paste
                 break;
         }
         cursorNibble = Nibble.UPPER;
-        super.keyTyped(typedChar, keyCode);
     }
 
     @Override
@@ -84,6 +134,10 @@ public class HexEditorGUI extends GuiContainerCharset<HexEditorContainer> {
     }
 
     public void setCursor(int cursor) {
+        if (!isShiftKeyDown() && !isCtrlKeyDown()) {
+            selectionEnd = 0;
+            selectionStart = 0;
+        }
         this.cursor = Math.max(0, Math.min(cursor, data.length - 1));
     }
 
@@ -109,8 +163,11 @@ public class HexEditorGUI extends GuiContainerCharset<HexEditorContainer> {
                 x += spacing;
             }
 
+            if (i >= selectionStart && i <= selectionEnd && selectionStart != selectionEnd) {
+                drawRect(x * charWidth, y * fontRenderer.FONT_HEIGHT, x * charWidth + 4 * charWidth, y * fontRenderer.FONT_HEIGHT + fontRenderer.FONT_HEIGHT, 0xff000000 | EnumDyeColor.GREEN.getColorValue());
+            }
             if (i == cursor) {
-                drawRect(x * charWidth, y * fontRenderer.FONT_HEIGHT, x * charWidth + 4 * charWidth, y * fontRenderer.FONT_HEIGHT + fontRenderer.FONT_HEIGHT, 0xff000000 | EnumDyeColor.RED.getColorValue());
+                // drawRect(x * charWidth, y * fontRenderer.FONT_HEIGHT, x * charWidth + 4 * charWidth, y * fontRenderer.FONT_HEIGHT + fontRenderer.FONT_HEIGHT, 0xff000000 | EnumDyeColor.RED.getColorValue());
                 int nx = x + (cursorNibble == Nibble.UPPER ? 0 : 1);
                 drawRect(nx * charWidth, (int) (y * fontRenderer.FONT_HEIGHT + fontRenderer.FONT_HEIGHT * 0.9f), nx * charWidth + 2 * charWidth, y * fontRenderer.FONT_HEIGHT + fontRenderer.FONT_HEIGHT, 0xff000000 | EnumDyeColor.BLUE.getColorValue());
             }
